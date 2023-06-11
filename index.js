c244783968d0422261dc1dfa200304a0538ca2c7
barakatimothy <timothybaraka39@gmail.com>
2023-06-11 06:59:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name : "john doe",
+    streetAddress : "Fortworth Dallas"
+};
+function updateEmployeeWithKeyAndValue(employee,key,val){
+   return {...employee ,[key]:val}   
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   employee[key] = value;
+   return employee;
+}
+newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,"name","Allan wilson")
+function  deleteFromEmployeeByKey(employee,key){
+    const cloneEmployee = {...employee};
+    delete cloneEmployee.name;
+    return cloneEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee.name;
+   return employee;
+}
+

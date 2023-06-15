2c0567c70f4b85a9ae196a2cebb6303168d3b0ea
barakatimothy <timothybaraka39@gmail.com>
2023-06-15 09:14:47 -0400

    complete!

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const employee = {
-    name : "john doe",
-    streetAddress : "Fortworth Dallas"
+   name : "john doe",
+   streetAddress : "Fortworth Dallas"
 };
 function updateEmployeeWithKeyAndValue(employee,key,val){
-   return {...employee ,[key]:val}   
+  return {...employee ,[key]:val}   
 }
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-   employee[key] = value;
-   return employee;
+  employee[key] = value;
+  return employee;
 }
 newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,"name","Allan wilson")
 function  deleteFromEmployeeByKey(employee,key){
-    const cloneEmployee = {...employee};
-    delete cloneEmployee.name;
-    return cloneEmployee;
+   const cloneEmployee = {...employee};
+   delete cloneEmployee.name;
+   return cloneEmployee;
 }
 function destructivelyDeleteFromEmployeeByKey(employee, key){
   delete employee.name;
-   return employee;
+  return employee;
 }
-
+ 
